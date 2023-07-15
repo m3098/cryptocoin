e49d9f0c2b4fa0de125d0573a33f94ec1fe14ea2
limnull <m.3098.off@gmail.com>
2023-07-15 15:49:00 +0300

    декомпозиция

--- lib/features/coin/view/coin_screen.dart
+++ lib/features/coin/view/coin_screen.dart
@@ -8,53 +8,76 @@ class CoinScreem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          const SizedBox(
-            height: 16,
-          ),
-          const SizedBox(
-            width: double.infinity,
-            child: Icon(
-              Icons.monetization_on,
-              size: 150,
-            ),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Text(
-            'BTC',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          TextSection(
-            child: Text(
-              '1232132131 \$',
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          const TextSection(
-              child: Column(
-            children: [
-              TextRow(
-                title: 'Hight 24 Hour',
-                value: '123213231213',
-              ),
-              TextRow(
-                title: 'Low 24 Hour',
-                value: '123213213',
-              ),
-            ],
-          ))
-        ]),
-      ),
+      body: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: CoinCard(
+              coinName: 'btc',
+              currentPrice: '21321.213',
+              hightPrice: '23213.312',
+              lowPrice: '123213.54')),
     );
+  }
+}
+
+class CoinCard extends StatelessWidget {
+  const CoinCard({
+    super.key,
+    required this.coinName,
+    required this.currentPrice,
+    required this.hightPrice,
+    required this.lowPrice,
+  });
+
+  final String coinName;
+  final String currentPrice;
+  final String hightPrice;
+  final String lowPrice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+      const SizedBox(
+        height: 16,
+      ),
+      const SizedBox(
+        width: double.infinity,
+        child: Icon(
+          Icons.monetization_on,
+          size: 150,
+        ),
+      ),
+      const SizedBox(
+        height: 24,
+      ),
+      Text(
+        coinName,
+        style: Theme.of(context).textTheme.headlineLarge,
+      ),
+      const SizedBox(
+        height: 18,
+      ),
+      TextSection(
+        child: Text(
+          '$currentPrice \$',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+      ),
+      const SizedBox(
+        height: 8,
+      ),
+      TextSection(
+          child: Column(
+        children: [
+          TextRow(
+            title: 'Hight 24 Hour',
+            value: hightPrice,
+          ),
+          TextRow(
+            title: 'Low 24 Hour',
+            value: lowPrice,
+          ),
+        ],
+      ))
+    ]);
   }
 }
--- lib/features/coin/widgets/widgets.dart
+++ lib/features/coin/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'text_section.dart';
 export 'text_row.dart';
+export 'coin_card.dart';
