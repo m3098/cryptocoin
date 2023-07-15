986521a5eedcfc9b2ec6473f6cc60880613116d8
limnull <m.3098.off@gmail.com>
2023-07-15 10:05:08 +0300

    добавлен экран коина

---
+++ lib/features/coin/view/coin_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CoinScreem extends StatelessWidget {
+  const CoinScreem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
